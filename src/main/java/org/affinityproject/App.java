85a59e61ee116d3cfaba95fd809013a4f052fe7e
Jian Wei Chai <jianwei.c@affinidi.com>
2021-08-22 21:19:42 +0800

    Fix

---
+++ src/main/java/org/affinityproject/App.java
@@ -0,0 +1,16 @@
+package org.affinityproject;
+import software.aws.jsiisamples.jsii.HelloWorld; 
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+   
+        HelloWorld h = HelloWorld();
+        System.out.println( "Hello World!" );
+    }
+}
